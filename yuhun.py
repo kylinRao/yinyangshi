1113224b99cc2d684cd744c93db06639ad1b8312
kylin <975168367@qq.com>
2017-05-29 18:19:22 +0800

    yuhun新增可空参数，战斗次数

--- yuhun.py
+++ yuhun.py
@@ -8,13 +8,21 @@ PATH = lambda p: os.path.abspath(
 
 class tansuoSigleTests(ContactsAndroidTests):
     def test_1_yu_hun(self):
+        count = 3
+        try:
+            if sys.argv[1]:
+
+                count = int(sys.argv[1])
+                print "count is {count}".format(count=count)
+        except:
+                pass
         loggerInner.info("------start yu hun!! ")
         start2TingYuan(self.driver)
         #点击探索探索
         loggerInner.info("------click search button at {x},{y} ".format(x=int(XMAX*53/100),y=int(YMAX/5)))
         self.driver.tap([(int(XMAX*53/100),int(YMAX/5))])
         sleep(5)
-        self.yu_hun(count=3)
+        self.yu_hun(count=count)
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(tansuoSigleTests)
     unittest.TextTestRunner(verbosity=2).run(suite)
